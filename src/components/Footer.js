6ed50dd1678e79239879d02a8e4fee2229e62401
Kirsy Ho <hosuthaophuoc@gmail.com>
2025-05-22 12:20:53 +1000

    Fix footer content and formatting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,46 +1,60 @@
-import { FaFacebook, FaHome, FaLinkedin, FaMailBulk, FaPhone } from "react-icons/fa"
-import "./FooterStyles.css"
+import {
+  FaFacebook,
+  FaHome,
+  FaLinkedin,
+  FaMailBulk,
+  FaPhone,
+} from "react-icons/fa";
+import "./FooterStyles.css";
 
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
     <div className="footer">
-        <div className="footer-container">
-            <div className="left">
-                <div className="location">
-                    <h4>
-                    <FaHome size={20} style={{color:"#fff", marginRight: "2rem"}} />
-                    Hawthorn, Melbourne
-                    </h4>
-                </div>
-                <div className="phone">
-                    <h4>
-                    <FaPhone size={20} style={{color:"#fff", marginRight: "2rem"}} />
-                    0415 330 620
-                    </h4>
-                </div>
-                <div className="email">
-                    <h4>
-                    <FaMailBulk size={20} style={{color:"#fff", marginRight: "2rem"}} />
-                    hosuthaophuoc@gmail.com
-                    </h4>
-                </div>
-            </div>
-            <div className="right">
-                <h4>Connect with me</h4>
-                <div className="social">
-                <FaFacebook size={20} style={{color:"#fff", marginRight: "2rem"}} />
-                hosuthaophuoc@gmail.com
-                <FaLinkedin size={20} style={{color:"#fff", marginRight: "2rem"}} />
-                hosuthaophuoc@gmail.com
-                </div>
-            </div>
+      <div className="footer-container">
+        <div className="left">
+          <div className="location">
+              <FaHome
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              <p>Hawthorn, Melbourne</p>
+          </div>
+          <div className="phone">
+              <FaPhone
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              <p>0415 330 620</p>
+          </div>
+          <div className="email">
+              <FaMailBulk
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              <p>hosuthaophuoc@gmail.com</p>
+          </div>
         </div>
+        <div className="right">
+          <h4>Connect with me</h4>
+          <div className="social">
+              <FaLinkedin
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              <a
+                href="https://www.linkedin.com/in/kirsy-phuoc-ho/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <p>Phuoc (Kirsy) Ho</p>
+              </a>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
-
-
+export default Footer;
